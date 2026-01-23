869006f1f34a4c746931f6d848a673c5f8c3a13e
GaoWeiChang <waylove01555@gmail.com>
2026-01-23 17:36:41 +0800

    add trigger test case

--- test/test_trigger_insert.sql
+++ test/test_trigger_insert.sql
@@ -10,7 +10,7 @@ CREATE TABLE sensor_data (
 
 SELECT create_hypertable('sensor_data', 'time', INTERVAL '1 day');
 
-INSERT INTO sensor_data VALUES ('2024-01-01 00:00:00+00'::timestamptz, 1, 25.5, 60.0);
+INSERT INTO sensor_data VALUES ('2024-01-01 00:00:00+00', 1, 25.5, 60.0);
 
 INSERT INTO sensor_data VALUES 
     ('2024-01-01 06:00:00+00', 1, 26.0, 61.0),
@@ -35,32 +35,20 @@ ORDER BY id;
 
 \echo 'Insert across multiple days...'
 
-INSERT INTO sensor_data VALUES ('2024-01-03 08:00:00+00'::timestamptz, 1, 24.0, 58.0);
-INSERT INTO sensor_data VALUES ('2024-01-03 16:00:00+00'::timestamptz, 2, 26.5, 60.5);
-INSERT INTO sensor_data VALUES ('2024-01-04 09:00:00+00'::timestamptz, 3, 27.0, 61.5);
-INSERT INTO sensor_data VALUES ('2024-01-04 15:00:00+00'::timestamptz, 1, 25.5, 59.5);
-INSERT INTO sensor_data VALUES ('2024-01-05 11:00:00+00'::timestamptz, 2, 28.5, 63.5);
+INSERT INTO sensor_data VALUES ('2024-01-03 08:00:00+00', 1, 24.0, 58.0);
+INSERT INTO sensor_data VALUES ('2024-01-03 16:00:00+00', 2, 26.5, 60.5);
+INSERT INTO sensor_data VALUES ('2024-01-04 09:00:00+00', 3, 27.0, 61.5);
+INSERT INTO sensor_data VALUES ('2024-01-04 15:00:00+00', 1, 25.5, 59.5);
+INSERT INTO sensor_data VALUES ('2024-01-05 11:00:00+00', 2, 28.5, 63.5);
+
+INSERT INTO sensor_data VALUES 
+    ('2024-01-03 08:00:00+00', 1, 24.0, 58.0),
+    ('2024-01-03 16:00:00+00', 2, 26.5, 60.5),
+    ('2024-01-04 09:00:00+00', 3, 27.0, 61.5),
+    ('2024-01-04 15:00:00+00', 1, 25.5, 59.5),
+    ('2024-01-05 11:00:00+00', 2, 28.5, 63.5);
 
 SELECT COUNT(*) as total_rows FROM sensor_data;
-
-
-\echo 'Test batch insert 100 rows...'
-
-\timing on
-
-INSERT INTO sensor_data 
-SELECT 
-    '2024-01-01 00:00:00+00'::timestamptz + (i || ' minutes')::interval as time,
-    (i % 5) + 1 as sensor_id,
-    20 + (random() * 10) as temperature,
-    50 + (random() * 20) as humidity
-FROM generate_series(1, 100) i;
-
-\timing off
-
-\echo 'Total rows after batch insert:'
-SELECT COUNT(*) FROM sensor_data;
-
 
 \echo 'Testing second hypertable with hourly chunks...'
 
@@ -77,6 +65,10 @@ INSERT INTO metrics VALUES ('2024-01-01 00:30:00+00', 100.0);
 INSERT INTO metrics VALUES ('2024-01-01 01:30:00+00', 150.0);
 INSERT INTO metrics VALUES ('2024-01-01 02:30:00+00', 200.0);
 
+INSERT INTO metrics VALUES
+    ('2024-01-01 00:30:00+00', 100.0),
+    ('2024-01-01 01:30:00+00', 150.0),
+    ('2024-01-01 02:30:00+00', 200.0);
 
 SELECT * FROM metrics ORDER BY time;
 
